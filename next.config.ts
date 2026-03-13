a747233edca86a091b4fc4dd1058cb232cdc5bb4
DoterDe <chil.aboka2008@gmail.com>
2026-03-13 08:05:25 +0500

    fix: clean next.config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
   // Разрешаем доступ с других устройств в локальной сети
   serverExternalPackages: [],
   experimental: {
-    allowedDevOrigins: ['192.168.1.70', 'localhost:3000'],
+    
   },
 };
 
